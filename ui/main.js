0b7a1bd93fcf496fc24b785983280f0a5a599414
Nithin C P <nithin.cp@hotmail.com>
2016-09-27 16:32:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 
 //for move the image
+
 var img = document.getElementById('img');
 img.onclik = function(){
     img.style.marginLeft = "100 px";
