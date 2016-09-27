e5fca91600d0df7c325ff7bdbe637f05a9200562
Nithin C P <nithin.cp@hotmail.com>
2016-09-27 16:30:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 
 //for move the image
-var img = document.getElementById('madi');
+var img = document.getElementById('img');
 img.onclik = function(){
     img.style.marginLeft = "100 px";
 };
