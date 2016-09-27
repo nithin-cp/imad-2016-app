b66448e4c1cd5a263bf3863de2e2d8a4cc249f4d
Nithin C P <cpnithin143@gmail.com>
2016-09-27 14:09:34 +0530

    add code of the html to avoid the repeating html codes.

--- server.js
+++ server.js
@@ -14,7 +14,37 @@ var articleOne={
     content:'       <p> This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article. </p>' 
 };
 
+var htmlTemplate = '
+<html>
+  <head>
+    <title>
+      ${title}
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link href="/ui/style.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="container">
+      <div>
+	<a href="/">Home</a>
+      </div>
+      <hr/>
+      <h3>
+	${heading}
+      </h3>
+      <div>
+	${date}
+      </div>
+      <div>
+       ${content}
+      </div>
+    </div>
+  </body>
+    
+</html>
 
+
+';
 
 
 
