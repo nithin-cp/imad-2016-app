a02b06385c37a3231c2d9b305a301f63e7444fa4
Nithin C P <nithin.cp@hotmail.com>
2016-09-27 14:04:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+
+var articleOne={
+    title : 'Article-one Nithincp',
+    heading: 'Article One',
+    date: 'sep 26, 2016',
+    content:'       <p> This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article. </p>' 
+};
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
