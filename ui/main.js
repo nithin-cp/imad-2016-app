432ee256f4c2b88d43e13fdaef760bfa763142bd
Nithin C P <nithin.cp@hotmail.com>
2016-09-30 14:09:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,28 +3,27 @@
 var button = document.getElementById('counter');
 var counter = 0;
 
-button.onclick = function(){
+button.onclick = function () {
     
     //create a request object
     
     var request = new XMLHttpRequest();
     // Capture the response and store it as a variable
     
-    request.onreadychangestate = fucntion(){
+    request.onreadychangestate = function(){
+        
         if(request.readyState === XMLHttpRequest.DONE){
             //take some action
             if (request.status === 200){
                 var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innnerHTML = counter.toString();
-    
-                
             }
         }
         //not done yet
     };
-    
     // make a request
     request.open('GET', 'http://nithin-cp.imad.hasura-app.io/counter', true);
     request.send(null); 
+    
 };
