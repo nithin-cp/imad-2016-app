6a7f849e2d8940ed0aa3f2288722e92085340f9e
Nithin C P <nithin.cp@hotmail.com>
2016-09-30 14:47:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ button.onclick = function () {
         //not done yet
     };
     // make a request
-    request.open('GET', 'http://nithin-cp.imad.hasura-app.io/counter', true);
+    request.open('GET', 'localhost:8080/counter', true);
     request.send(null); 
     
 };
